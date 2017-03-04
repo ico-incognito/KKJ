fd0cfdb08ef133ee0f0e02f48dbe5a940d06a4fa
Japanwala <Japanwala>
2017-03-04 22:36:12 +0530

    Done

---
+++ routes/regisform.js
@@ -0,0 +1,23 @@
+/**
+ * Created by GIGABYTE on 05-Feb-17.
+ */
+var express = require('express');
+var router = express.Router();
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('regisform');
+});
+
+router.post('/submit', function(req, res){
+    console.log(req.body);
+    res.send("You sent the name " + req.body.fname + " " + req.body.lname);
+
+});
+
+
+
+
+
+module.exports = router;
